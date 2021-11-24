752e5873d3a8994ae629a6ba4e41ace8e40928d0
SAKARIA-DIARRASSOUBA <77932880+SAKARIA-DIARRASSOUBA@users.noreply.github.com>
2021-11-24 18:09:40 +0100

    application

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request
+import pandas as pd
+import numpy as np
+import joblib 
+
+with open('classifier.model','rb') as file:
+
+    model=joblib.load(file)
+
+with open("MinMaxScaler","rb") as f:
+    normalizer=joblib.load(f)
+
+print("flask version",flask.__version__)
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+	return render_template('main.html')
+
+decision=[ "Bad to reversing","Good to reversing"]
+
+@app.route('/', methods=['POST','GET'])
+def main(): 
+    if request.method == 'GET':
+        return (flask.render_template('main.html'))
+
+    if request.method == 'POST':
+        
+        data=[float(x) for x in request.form.values()]
+        array_features = np.array([data])
+        array_features.reshape(-1,1)
+        normalize_features=normalizer.transform(array_features)
+        prediction = model.predict(normalize_features)
+        output = round(prediction[0])
+        
+       
+    return render_template('main.html', 
+         original_input={'GP':data[0],'PTS':data[1],'FGPer':data[2],'3P_Made':data[3], 
+         '3PPre':data[4],'FTM':data[5],'FTPer':data[6],'REB':data[7],
+         'AST':data[8],'STL':data[9],'BLK':data[10],'TOV':data[11]} ,
+        result=decision[output] )
+
+
+                              
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
